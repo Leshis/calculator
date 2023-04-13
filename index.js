77431f2b4c57ec88412a1ba0caa86502fb3c35de
Leshis <94430516+Leshis@users.noreply.github.com>
2023-04-13 13:51:51 +0100

    Created basic functions and interface

---
+++ index.js
@@ -0,0 +1,25 @@
+const num1 = 10;
+const num2 = 4;
+const operation = 'subtract';
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1/num2;
+}
+
+function operate(operation, num1, num2){
+    return (window[operation](num1, num2));
+}
+
+console.log(operate(operation, num1, num2));
