544895b7edb83f51f151541b46b5ad6e295ca531
Leshis <94430516+Leshis@users.noreply.github.com>
2023-04-13 17:03:50 +0100

    Improved functionality

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const screen = document.getElementById('screen');
+const screenUpper = document.getElementById('screen-upper');
+const screenLower = document.getElementById('screen-lower');
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
 
 let numString = '';
 let num1 = 0;
 let num2 = 0;
+let answer = null;
 let operation = '';
+let operationChar = '';
+let operationClicked = false;
+let shouldReset = false;
 
 function add(num1, num2) {
     return num1 + num2;
@@ -22,59 +29,65 @@ function divide(num1, num2) {
 }
 
 function operate(operation, num1, num2) {
-    screen.textContent = window[operation](num1, num2);
-    return (window[operation](num1, num2));
-}
-
-function setNum1() {
-    num1 = parseInt(numString);
+    answer = window[operation](num1, num2);
+    screenLower.textContent = answer;
+    num2 = 0;
     numString = '';
 }
 
-function setNum2() {
-    num2 = parseInt(numString);
+function reset() {
+    num1 = 0;
+    num2 = 0;
     numString = '';
+    screenUpper.textContent = '';
+    screenLower.textContent = '';
 }
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        if (screenLower.textContent === '0' || shouldReset) {
+            screenLower.textContent = '';
+            shouldReset = false;
+        }
+        if (operationClicked) {
+            screenLower.textContent = '';
+            operationClicked = false;
+        }
+        numString += e.target.textContent;
+        screenLower.textContent += e.target.textContent;
+    });
+});
 
 document.body.addEventListener('click', function (e) {
-    if (!isNaN(e.target.textContent)) {
-        numString += e.target.textContent;
-        screen.textContent += e.target.textContent;
-    }
-    else if (e.target.id === 'add') {
-        setNum1();
-        operation = e.target.id;
-        screen.textContent += ' + ';
-        return [num1, operation];
-    }
-    else if (e.target.id === 'subtract') {
-        setNum1();
-        operation = e.target.id;
-        screen.textContent += ' - ';
-        return [num1, operation];
-    }
-    else if (e.target.id === 'multiply') {
-        setNum1();
-        operation = e.target.id;
-        screen.textContent += ' * ';
-        return [num1, operation];
-    }
-    else if (e.target.id === 'divide') {
-        setNum1();
-        operation = e.target.id;
-        screen.textContent += ' / ';
-        return [num1, operation];
-    }
-    else if (e.target.id === 'delete') {
-        num1 = 0;
-        num2 = 0;
-        numString = '';
-        screen.textContent = '';
-        return [num1, num2];
-    }
-    else if (e.target.id === 'equals') {
-        setNum2();
-        operate(operation, num1, num2);
-    }
-
+    switch (e.target.id) {
+        case 'add':
+        case 'subtract':
+        case 'multiply':
+        case 'divide':
+          if (!answer) {
+            num1 = parseFloat(numString);
+            numString = '';
+          } else if (answer) {
+            num1 = answer;
+            numString = '';
+          }
+          shouldReset = true;
+          operation = e.target.id;
+          operationChar = e.target.textContent;
+          screenUpper.textContent = `${num1} ${operationChar}`;
+          break;
+        case 'delete':
+          reset();
+          break;
+        case 'equals':
+          if (numString) {
+            num2 = parseFloat(numString);
+            numString = '';
+            screenUpper.textContent = `${num1} ${operationChar} ${num2} =`
+            operate(operation, num1, num2);
+          }
+          break;
+        default:
+          break;
+      }
 });
