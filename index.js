828aaa701fa97dc24beba3c7f57f3b8dd988bcf2
Leshis <94430516+Leshis@users.noreply.github.com>
2023-04-14 10:38:58 +0100

    Prevent dividing by zero

--- index.js
+++ index.js
@@ -25,12 +25,19 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    if(num2 === 0){
+        return 'Cannot divide by zero';
+    }
+    else {
+        return num1 / num2;
+    }
+    
 }
 
 function operate(operation, num1, num2) {
     answer = window[operation](num1, num2);
     screenLower.textContent = answer;
+    num1 = answer;
     num2 = 0;
     numString = '';
 }
@@ -46,6 +53,15 @@ function reset() {
     screenLower.textContent = '0';
 }
 
+function updateDisplay() {
+    if (num1 != 0) {
+        screenUpper.textContent = `${num1} ${operationChar}`;
+    }
+    if (numString !== '') {
+        screenLower.textContent = numString;
+    } 
+}
+
 numberButtons.forEach(button => {
     button.addEventListener('click', function (e) {
         if (screenLower.textContent === '0' || shouldReset) {
@@ -56,8 +72,11 @@ numberButtons.forEach(button => {
             screenLower.textContent = '';
             operationClicked = false;
         }
-        numString += e.target.textContent;
-        screenLower.textContent += e.target.textContent;
+        if (e.target.textContent === '.' && numString.includes('.')) return;
+        if (numString.length < 15) {
+            numString += e.target.textContent;
+            updateDisplay();
+        }
     });
 });
 
@@ -68,16 +87,20 @@ document.body.addEventListener('click', function (e) {
         case 'multiply':
         case 'divide':
             if (!answer) {
-                num1 = parseFloat(numString);
-                numString = '';
-            } else if (answer) {
+                if (numString !== '') {
+                    num1 = parseFloat(numString);
+                    numString = '';
+                }
+            }
+            else if (answer) {
                 num1 = answer;
                 numString = '';
             }
             shouldReset = true;
             operation = e.target.id;
             operationChar = e.target.textContent;
-            screenUpper.textContent = `${num1} ${operationChar}`;
+            operationClicked = true;
+            updateDisplay();
             break;
         case 'delete':
             reset();
